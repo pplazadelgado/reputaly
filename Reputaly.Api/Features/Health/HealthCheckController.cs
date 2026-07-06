35c71051a55307a019d2fe0ac12b15b93966123d
Pedro J. Plaza <pplazadelgado@gmail.com>
2026-07-06 18:55:30 +0200

    Healt

---
+++ Reputaly.Api/Features/Health/HealthCheckController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reputaly.API.Features.Health;
+
+[ApiController]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet("/health")]
+    public IActionResult Handle() => Ok(new { status = "ok" });
+}
