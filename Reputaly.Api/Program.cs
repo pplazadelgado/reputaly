8a33e732e62b6476b493ec9abf5f464189a1b164
Pedro J. Plaza <pplazadelgado@gmail.com>
2026-07-06 18:21:46 +0200

    Docker

--- Reputaly.Api/Program.cs
+++ Reputaly.Api/Program.cs
@@ -104,7 +104,11 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("FrontendDev", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        var allowedOrigins = builder.Configuration
+            .GetSection("Cors:AllowedOrigins")
+            .Get<string[]>() ?? new[] { "http://localhost:5173" };
+
+        policy.WithOrigins(allowedOrigins)
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
